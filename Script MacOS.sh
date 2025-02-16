2f6fb94307d85f9a6ee633db1936f83b75dcc332
MaronNeo6 <eduardodantasnery@gmail.com>
2025-02-16 13:09:28 -0300

    modified:   Script MacOS.sh

--- Script MacOS.sh
+++ Script MacOS.sh
@@ -4,68 +4,44 @@ echo "Este script demanda o Brew esteja instalado para funcionar"
 echo "Caso o contrario instale"
 
 mostrar_menu() {
-    echo "Escolha uma opção"
-    echo "1. Instalar o Neovim"
-    echo "2. Pular"
-    echo "3. Sair"
+    echo "Qual Editor você deseja instalar?"
+    echo "1. NeoVim com LazyVim"
+    echo "2. VSCodium"
+    echo "3. Pular"
+    echo "4. Sair"
 }
 
 while true do
     mostrar_menu
     read -p "Digite sua escolha: " escolha
 
-    case $escolha in
+    case $scolha in
         1)
-            echo "Instalando o Neovim"
+            echo "Instalando o NeoVim com LazyVim"
             brew install neovim
-            break
-            ;;
-        2)
-            echo "Pulando a etapa"
-            break
-            ;;
-        3)
-            echo "Saindo do Script..."
-            break
-            exit
-            ;;
-        *)
-            echo "Opção inválida, tente novamente."
-            ;;
-    esac
-done
-
-mostrar_menu() {
-    echo "Escolha uma opção"
-    echo "1. Instalar o Lazyvim"
-    echo "2. Pular"
-    echo "3. Sair"
-}
-
-while true do
-    mostrar_menu
-    read -p "Digite sua eescolha: " escolha
-
-    case $escolha in
-        1)
             brew install git
             git clone https://github.com/LazyVim/starter ~/.config/nvim
             rm -rf ~/.config/nvim/.git
-            nvim
+            neovim
             break
             ;;
         2)
-            echo "Pulando a etapa"
+            echo "Instalando o VSCodiuim"
+            brew install --cask vscodium
             break
             ;;
         3)
-            echo "Saindo do Script..."
+            echo "Pulando a etapa"
+            break
+            ;;
+        4)
+            echo "Fechando o Script..."
             break
             exit
             ;;
         *)
             echo "Opção inválida, tente novamente"
-            ;;        
+            ;;
     esac
 done
 
