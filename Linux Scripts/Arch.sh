c2bd2bdcb1bc69247336af41b6b666cdbba72795
MaronNeo6 <eduardodantasnery@gmail.com>
2025-12-22 13:26:07 -0300

    modified:   Linux Scripts/Arch.sh

--- Linux Scripts/Arch.sh
+++ Linux Scripts/Arch.sh
@@ -153,7 +153,8 @@ done
 clear
 
 echo "Instalando o Sober (Roblox no Linux)"
-flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
+flatpak install flathub org.vinegarhq.Sober
+clear
 
 MENU="
 1 - Instalar o ZSH
